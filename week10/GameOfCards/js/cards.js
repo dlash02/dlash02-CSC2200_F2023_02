6599ec02a5a75c7c2853ab55c77d1f9f3d7518a8
David Lash <dlash02@gmail.com>
2023-10-31 14:55:52 -0500

    Update cards.js

--- week10/GameOfCards/js/cards.js
+++ week10/GameOfCards/js/cards.js
@@ -1,3 +1,22 @@
+let deck = {
+    cards : [
+        { 'img' : '2C.png', 'value': 2, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '2D.png', 'value': 2, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '2H.png', 'value': 2, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '2S.png', 'value': 2, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '3C.png', 'value': 3, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '3D.png', 'value': 3, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '3H.png', 'value': 3, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '3S.png', 'value': 3, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '4C.png', 'value': 4, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '4D.png', 'value': 4, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '4H.png', 'value': 4, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '4S.png', 'value': 4, 'suit' : 'spades', 'dealt': false}
+
+    ]
+}
 function startGame() {
     alert( "Starting games")
 }
