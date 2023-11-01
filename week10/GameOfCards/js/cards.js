0bec2d6313fbc0d8d11f51cac98675f83ba32b8b
David Lash <dlash02@gmail.com>
2023-10-31 19:41:51 -0500

    Update cards.js

--- week10/GameOfCards/js/cards.js
+++ week10/GameOfCards/js/cards.js
@@ -13,8 +13,32 @@ let deck = {
         { 'img' : '4C.png', 'value': 4, 'suit' : 'clubs', 'dealt': false},
         { 'img' : '4D.png', 'value': 4, 'suit' : 'diamonds', 'dealt': false},
         { 'img' : '4H.png', 'value': 4, 'suit' : 'hearts', 'dealt': false},
-        { 'img' : '4S.png', 'value': 4, 'suit' : 'spades', 'dealt': false}
+        { 'img' : '4S.png', 'value': 4, 'suit' : 'spades', 'dealt': false},
 
+        { 'img' : '5C.png', 'value': 5, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '5D.png', 'value': 5, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '5H.png', 'value': 5, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '5S.png', 'value': 5, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '6C.png', 'value': 6, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '6D.png', 'value': 6, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '6H.png', 'value': 6, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '6S.png', 'value': 6, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '7C.png', 'value': 7, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '7D.png', 'value': 7, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '7H.png', 'value': 7, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '7S.png', 'value': 7, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '8C.png', 'value': 8, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '8D.png', 'value': 8, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '8H.png', 'value': 8, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '8S.png', 'value': 8, 'suit' : 'spades', 'dealt': false},
+
+        { 'img' : '9C.png', 'value': 9, 'suit' : 'clubs', 'dealt': false},
+        { 'img' : '9D.png', 'value': 9, 'suit' : 'diamonds', 'dealt': false},
+        { 'img' : '9H.png', 'value': 9, 'suit' : 'hearts', 'dealt': false},
+        { 'img' : '9S.png', 'value': 9, 'suit' : 'spades', 'dealt': false}
     ]
 }
 function startGame() {
