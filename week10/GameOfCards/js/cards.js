9bf45f5f2524ed955ac584e39e52ca8f926bbf5b
David Lash <dlash02@gmail.com>
2023-11-02 19:15:54 -0500

    Update carsds.js 11/2
    Up

--- week10/GameOfCards/js/cards.js
+++ week10/GameOfCards/js/cards.js
@@ -39,8 +39,83 @@ let deck = {
         { 'img' : '9D.png', 'value': 9, 'suit' : 'diamonds', 'dealt': false},
         { 'img' : '9H.png', 'value': 9, 'suit' : 'hearts', 'dealt': false},
         { 'img' : '9S.png', 'value': 9, 'suit' : 'spades', 'dealt': false}
-    ]
+    ],
+    dealtCt : 0,
+    getCard : function(){
+
+        let gotGoodCard = false;
+        while ( !gotGoodCard ){
+            let rNum = Math.floor( Math.random() * this.cards.length );
+            let card = deck.cards[rNum];
+            if ( !card.dealt ){
+                card.dealt = true;
+                this.dealtCt += 1;
+                return deck.cards[rNum];
+            } else if ( this.dealtCt == deck.cards.length ){
+                //ToDo: Fix this to look into existing hands
+                let resp = prompt("Out of cards ... shall I reshuffle?");
+                return null;
+            }
+        }
+        return card;
+    }
+}
+let User = {
+    bet : 0,
+    hand : [] // array of cards
+
+}
+let Game = {
+    nCards : 5,  // cards in a hanud
+    buildAHand : function() {
+        let h = [];
+        for ( let i=0; i<this.nCards; i++){
+            let c = deck.getCard();
+            // console.log( c );
+            h.push( c );
+        }
+        return h;
+    },
+    gotPair : function( hand ) {
+        let handValues = [];
+        // LEFT OFF HERE -> Use a map!
+        for ( let i=0; i<hand.length; i++ ){
+            let v = hand[i].value;
+
+        }
+    }
+}
+let UI = {
+    getBet : function ( betId, betDisId ){
+        //ToDo: Implement a MAX_BET
+        let betObj = document.getElementById( betId );
+        let bet = betObj.value;
+        let msg = ""
+        let notGotError = true;
+        if ( bet <= 0 ||  isNaN( bet ) || bet == undefined || bet == null ) {
+            msg = `<span style='color:red'> Illegal Bet:${bet} </span> `;
+            notGotError = false;
+        } else {
+            msg = `<span style='color:blue'>  Bet:${bet} </span> `;
+        }
+        document.getElementById(betDisId).innerHTML = msg;
+        return notGotError;
+    },
+    displayHand( hand, id ){
+        let resObj = document.getElementById( id );
+        let oStr = "";
+        for ( let i=0; i<hand.length; i++ ){
+            let c = hand[i];
+            oStr += `<img src=imgs/${c.img} width='50px' alt="Card">`
+        }
+        resObj.innerHTML = oStr;
+    }
 }
 function startGame() {
-    alert( "Starting games")
+    // alert( "Starting games")
+    if ( UI.getBet( "uBet","Results" )) {
+        let daCard = deck.getCard();
+        User.hand = Game.buildAHand();
+        UI.displayHand( User.hand, "Cards");
+    }
 }
